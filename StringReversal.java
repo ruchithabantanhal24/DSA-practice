d7de5b5f2cb103c792a46dc0a1276f5569ebad31
Ruchitha Bantanhal <ruchithabantanhal@gmail.com>
2026-02-12 15:26:19 +0530

    Reverse

---
+++ StringReversal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class StringReversal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i = s.length()-1; i>=0 ;i--){
+            sb.append(s.charAt(i));
+        }
+        System.out.println(sb.toString());
+    }
+}
