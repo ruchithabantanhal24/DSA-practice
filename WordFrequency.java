ec07d15825f6b6b5c9c956115a216d9669e33648
Ruchitha Bantanhal <ruchithabantanhal@gmail.com>
2026-03-18 18:22:21 +0530

    count frequency of a word

---
+++ WordFrequency.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class WordFrequency {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = "I am a good girl and a good girl suffers a lot";
+        s = s.toLowerCase();
+        int count = 0;
+
+        String str = sc.next();
+        
+        String[] arr = s.split(" ");
+
+        for(String word : arr){
+            if(word.equals(str)){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
