ed56d6b533858c498ae8212878fa2c90a26e8b3f
ruchithabantanhal24 <ruchithabantanhal155@gmail.com>
2026-03-18 18:25:44 +0530

    Update WordFrequency.java

--- WordFrequency.java
+++ WordFrequency.java
@@ -5,11 +5,8 @@ public class WordFrequency {
         String s = "I am a good girl and a good girl suffers a lot";
         s = s.toLowerCase();
         int count = 0;
-
         String str = sc.next();
-        
         String[] arr = s.split(" ");
-
         for(String word : arr){
             if(word.equals(str)){
                 count++;
